ea185b675cd29631b668f5de28e01bc7e69ee964
albertjan <albertjan@curit.com>
2013-11-03 10:39:18 +0100

    dingetjes met persona

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -1,15 +1,15 @@
 Citycrush.ApplicationController = Ember.Controller.extend({
     init: function () {
         this._super();
-        console.log(this.get('model'));
-        navigator.id.watch({
-            loggedInUser: this.get('email'),
-            onlogin: function (assertion) {
-                console.log('login');
-            },
-            onlogout: function () {
-                console.log('logout');
-            }
-        });
+//            navigator.id.watch({
+//                loggedInUser: this.get('email'),
+//                onlogin: function (assertion) {
+//                    console.log('login');
+//                },
+//                onlogout: function () {
+//                    console.log('logout');
+//                }
+//            });
     }
+    
 });
--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -1,9 +1,18 @@
 Citycrush.ApplicationRoute = Ember.Route.extend({
+    init: function(){
+    },
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        var user = this.store.find('user', 1);//.get('firstObject');
-        console.log(user.get('email'));
-        return user;
+        var user, that = this;
+        
+        user = navigator.id.get(function(e){
+            var luser = this.store.find('user', {email: e.email});//.get('firstObject');
+            user.then(function(){
+                console.log(user.get('email'));
+            });
+            return luser;
+        });
+        return user; 
     }
 });
