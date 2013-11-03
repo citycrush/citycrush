a49818759363f3335371e76fd16814b19250e920
Remko Boschker <remko@informatietuin.nl>
2013-11-03 15:21:28 +0100

    lege avatar binden

--- app/scripts/controllers/register_controller.js
+++ app/scripts/controllers/register_controller.js
@@ -1,9 +1,17 @@
 Citycrush.RegisterController = Ember.ObjectController.extend({
+    
+    selectedAvatar: function(){
+        var avatarUrl = this.get('avatar');
+        if (avatarUrl === undefined || avatarUrl === null || avatarUrl === "")
+            return "/images/lege-avatar.png"
+        else {
+            return avatarUrl;
+        } 
+    }.observes('avatar'),
+    
   actions: {
     selectAvatar: function (avatar) {
-        this.set('avatar', avatar);  
-        $(this).toggleClass('selected');
-        console.log($(this));
+        this.set('avatar', avatar);
     }
   }
 });
